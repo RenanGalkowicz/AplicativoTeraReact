6420fe3350c1a3af3d2f49a793abd2c74dae682e
Renan Galkowicz <cursosgalk@gmail.com>
2023-06-12 21:54:00 -0300

    Adiciona navegação

--- src/components/molecules/DrawerMenu.jsx
+++ src/components/molecules/DrawerMenu.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 export default function DrawerMenu(props) {
   return (
-    <Drawer anchor="right" open={props.open} onClose={() => console.log("ok")}>
-      Menu
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClose={() => props.setOpen(false)}
+    >
+      <List>
+        <ListItem>
+          <Link to="/">Home</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/users">Usuários</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/users/1/post">Criar Post</Link>
+        </ListItem>
+      </List>
     </Drawer>
   );
 }
